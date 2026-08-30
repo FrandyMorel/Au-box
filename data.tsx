5ff5fb057822d74c4c0d021def58c20021eb67fe
Frandy Morel <frandymorel3@gmail.com>
2026-08-29 22:18:00 -0400

    Se agragaron campos correspondientes a la estructura

---
+++ data.tsx
@@ -0,0 +1,20 @@
+export const itemSidebar = [
+    {
+        id: 1,
+        title: "Dashboard",
+        icon: ,
+        link: "/dashboard",
+    },
+    {
+        id: 2,
+        title: "Automatizaciones",
+        icon: ,
+        link: "/automatizaciones",
+    },
+    {
+        id: 3,
+        title: "Incidencias",
+        icon: ,
+        link: "/incidencias",
+    },
+];
